ee73a583cb002c3f588b039e6851602d836127ff
Florian Krupicka <krupicka@synyx.de>
2019-06-16 21:41:20 +0200

    Add some more example endpoints with different security requirements

--- src/main/java/rocks/coffeenet/examples/oidc/IndexController.java
+++ src/main/java/rocks/coffeenet/examples/oidc/IndexController.java
@@ -2,11 +2,17 @@ package rocks.coffeenet.examples.oidc;
 
 import org.springframework.http.MediaType;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -16,9 +22,35 @@ import java.security.Principal;
 @RequestMapping("/")
 public class IndexController {
 
-    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
-    public String index(Principal p) {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> index() {
 
-        return "Hello world!";
+        return Map.of("hello", "world");
+    }
+
+
+    @GetMapping(value = "/authentication-needed", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> authenticationNeeded(Principal p) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("principal", p);
+        result.put("authentication", authentication);
+
+        return result;
+    }
+
+
+    @GetMapping(value = "/authentication-not-needed", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> authenticationNotNeeded(Principal p) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("principal", p);
+        result.put("authentication", authentication);
+
+        return result;
     }
 }
--- src/main/java/rocks/coffeenet/examples/oidc/OidcExampleApplication.java
+++ src/main/java/rocks/coffeenet/examples/oidc/OidcExampleApplication.java
@@ -2,6 +2,13 @@ package rocks.coffeenet.examples.oidc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -10,5 +17,28 @@ public class OidcExampleApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(OidcExampleApplication.class, args);
+    }
+
+    @Configuration
+    static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+
+            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        }
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            //J-
+            //@formatter:off
+            http.authorizeRequests()
+                .antMatchers("/authentication-needed").authenticated()
+                .antMatchers("/authentication-not-needed").anonymous();
+            //@formatter:on
+            //J+
+        }
     }
 }
